f2f498830cd519adb300169a34a468590ce4d70b
unker <kfi.unknown@gmail.com>
2023-10-01 00:13:23 +0300

    changed server url

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,4 @@ import '../components/Polling/Polling.css';
 import Polling from '../components/Polling/Polling';
 
 const pollingContainer = document.querySelector('.polling-container');
-const polling = new Polling(process.env.SERVER_URL || 'https://ahj-homeworks-rxjs-server.onrender.com', pollingContainer);
+const polling = new Polling(process.env.SERVER_URL || 'https://unker-ahj-homeworks-rxjs-server.onrender.com', pollingContainer);
